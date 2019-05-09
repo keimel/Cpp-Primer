e333b9aa4ed1d85f69de6b090bb30e49b43e9664
keimel <47718047+keimel@users.noreply.github.com>
2019-05-09 17:09:01 +0200

    Update and rename 2.33.cpp to 2.33+2.34.cpp

--- Chapter 2/2.33+2.34.cpp
+++ Chapter 2/2.33+2.34.cpp
@@ -1,6 +1,11 @@
 /*
 [2.33]Using the variable definitions from this section, determine
 what happens in each of these assignments
+[2.34]Write a program containing the variables and assignments
+from the previous exercise. Print the variables before and after the
+assignments to check whether your predictions in the previous exercise were
+correct. If not, study the examples until you can convince yourself you know
+what led you to the wrong conclusion.
 */
 #include <iostream>
 
@@ -14,14 +19,15 @@ int main()
   auto d = &i; 
   auto e = &ci;
   auto &g = ci;
+  std::cout << a << '\\' << b << '\\' << c << '\\' << d << '\\' << e << '\\' << g << std::endl;
   
   a = 42;//a has type int and becomes 42
   b = 42;//b has type int and becomes 42
   c = 42;//c has type int and becomes 42
 //  d = 42;//d is pointer to int, cannot be assigned a literal
-//  e = 42;//e is pointer to const int, cannot be assigned a litteral
+//  e = 42;//e is pointer to const int, cannot be assigned a literal
 //  g = 42;//g is a reference of type const int and cannot be assigned different values
   
-  std::cout << a << '\n' << b << '\n' << c << std::endl;
+  std::cout << a << '\\' << b << '\\' << c << '\\' << d << '\\' << e << '\\' << g << std::endl;
   return 0;
 }
